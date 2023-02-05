874b35960982b2350ab5ec9a109c404f05d6edf5
blizrys <jirarotej@gmail.com>
2023-02-05 20:21:13 +1100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import Portfolio from './portfolio/Portfolio';
 
 function Copyright() {
   return (
@@ -20,14 +21,17 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Portfolio>
+      
+    </Portfolio>
+    // <Container maxWidth="sm">
+    //   <Box sx={{ my: 4 }}>
+    //     <Typography variant="h4" component="h1" gutterBottom>
+    //       Create React App example
+    //     </Typography>
+    //     <ProTip />
+    //     <Copyright />
+    //   </Box>
+    // </Container>
   );
 }
